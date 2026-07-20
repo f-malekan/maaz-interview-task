dd0a20cac8d7e36cf032d930e85bce8692206944
f-malekan <fateme.malekan96@gmail.com>
2026-07-20 22:34:56 +0330

    feat: add base dropdown

---
+++ app/components/baseComponents/BaseDropdown.vue
@@ -0,0 +1,33 @@
+<script setup lang="ts">
+defineProps<{
+  title: string;
+}>();
+
+const isOpen = ref(false);
+</script>
+
+<template>
+  <div
+    class="bg-white rounded-3xl shadow-[0_2px_3px_0_rgba(0,0,0,0.03)] overflow-hidden transition-all duration-300"
+    :class="[isOpen ? 'max-h-80' : 'max-h-12']"
+  >
+    <button
+      type="button"
+      class="flex w-full items-center justify-between p-4"
+      @click="isOpen = !isOpen"
+    >
+      <span class="text-gray-700 text-[13px] leading-4 font-medium">
+        {{ title }}
+      </span>
+
+      <DownIcon
+        :class="[
+          'text-gray-500 w-4 h-4 transition-transform duration-300',
+          { 'rotate-180': isOpen },
+        ]"
+      />
+    </button>
+
+    <slot />
+  </div>
+</template>
