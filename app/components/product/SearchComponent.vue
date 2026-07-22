7682bb7422cb572d23b17c5cd741388b8f1cd228
f-malekan <fateme.malekan96@gmail.com>
2026-07-23 01:07:20 +0330

    fix: add remove function and change value handling

--- app/components/product/SearchComponent.vue
+++ app/components/product/SearchComponent.vue
@@ -1,5 +1,15 @@
 <script setup lang="ts">
-const model = defineModel<string>();
+const searchValue = ref("");
+
+const showRemoveButton = computed(() => searchValue.value.length);
+
+const handleRemoveSearch = () => {
+  searchValue.value = "";
+};
+
+const emit = defineEmits<{
+  (e: "search", value: string): void;
+}>();
 </script>
 
 <template>
@@ -7,16 +17,27 @@ const model = defineModel<string>();
     <span class="text-gray-700 text-[13px] leading-4 font-medium"
       >فیلتر و جستجو</span
     >
-    <BaseInput v-model="model" class="my-4">
+    <BaseInput v-model="searchValue" class="my-4">
       <template #prepend>
         <SearchIcon
           class="w-7 h-6 py-1 text-primary border-l pl-3 border-dark-border ml-4"
         />
       </template>
       <template #append>
-        <CloseIcon class="w-4 h-4 text-gray-500" />
+        <button
+          v-show="showRemoveButton"
+          class="cursor-pointer"
+          @click="handleRemoveSearch"
+        >
+          <CloseIcon class="w-4 h-4 text-gray-500" />
+        </button>
       </template>
     </BaseInput>
-    <BaseButton label="جستجو" class="w-full" size="md" />
+    <BaseButton
+      label="جستجو"
+      class="w-full"
+      size="md"
+      @click="emit('search', searchValue)"
+    />
   </div>
 </template>
