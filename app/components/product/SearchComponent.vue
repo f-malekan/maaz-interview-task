4a45b21f611c81e2004e8d9396cd860eb183871c
f-malekan <fateme.malekan96@gmail.com>
2026-07-22 05:25:09 +0330

    feat: add search component

---
+++ app/components/product/SearchComponent.vue
@@ -0,0 +1,22 @@
+<script setup lang="ts">
+const model = defineModel<string>();
+</script>
+
+<template>
+  <div class="bg-white p-4 rounded-3xl shadow-[0_2px_3px_rgb(0_0_0/3%)]">
+    <span class="text-gray-700 text-[13px] leading-4 font-medium"
+      >فیلتر و جستجو</span
+    >
+    <BaseInput v-model="model" class="my-4">
+      <template #prepend>
+        <SearchIcon
+          class="w-7 h-6 py-1 text-primary border-l pl-3 border-dark-border ml-4"
+        />
+      </template>
+      <template #append>
+        <CloseIcon class="w-4 h-4 text-gray-500" />
+      </template>
+    </BaseInput>
+    <BaseButton label="جستجو" class="w-full" size="md" />
+  </div>
+</template>
